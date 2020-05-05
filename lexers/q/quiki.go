2fc850b0da8759baaf22b1050053805a7ee0521b
Mitchell Cooper <githubspam@rlygd.net>
2020-05-04 21:39:55 -0700

    map and list stuff

--- lexers/q/quiki.go
+++ lexers/q/quiki.go
@@ -21,6 +21,12 @@ var Quiki = internal.Register(MustNewLexer(
 			// start a comment
 			{`/\*`, CommentMultiline, Push("nested-comment")},
 
+			// start a map{} or map-based block
+			{`(map)(\s*)(\{)`, ByGroups(NameFunction, Text, Punctuation), Push("map")},
+
+			// start a list{} or list-based block
+			{`(list)(\s*)(\{)`, ByGroups(NameFunction, Text, Punctuation), Push("list")},
+
 			// start a generic block
 			{`([\w\-\$\.]+)(\s*)(\{)`, ByGroups(NameFunction, Text, Punctuation), Push("block")},
 		},
@@ -44,6 +50,25 @@ var Quiki = internal.Register(MustNewLexer(
 
 			/* exit block OR brace escape */
 			{`\}`, Punctuation, Pop(1)},
+		},
+
+		// rules inside a map{} or map-based block
+		"map": {
+			Include("block"),
+			{`:`, Punctuation, Push("value")},
+		},
+
+		// rules inside a list{} or list-based block
+		"list": {
+			Include("block"),
+			Default(Push("value")), // enter value
+		},
+
+		// rules inside a map or list value
+		"value": {
+			{`(?<!\\);`, Punctuation, Pop(1)}, // exit the value with unescaped semicolon
+			{`(?<!\\)}`, Punctuation, Pop(2)}, // exit the value AND block with unescaped closing brace
+			{`.+?`, String, nil},              // content in the value
 		},
 	},
 ))
