ec8ab152ba02c7b930e3dfb62133111f86f6e5b8
Mitchell Cooper <githubspam@rlygd.net>
2020-05-04 21:13:11 -0700

    block stuff

--- lexers/q/quiki.go
+++ lexers/q/quiki.go
@@ -14,14 +14,36 @@ var Quiki = internal.Register(MustNewLexer(
 		DotAll:    true,
 	},
 	Rules{
+
+		// rules at root level
 		"root": {
+
+			// start a comment
 			{`/\*`, CommentMultiline, Push("nested-comment")},
+
+			// start a generic block
+			{`([\w\-\$\.]+)(\s*)(\{)`, ByGroups(NameFunction, Text, Punctuation), Push("block")},
 		},
+
+		// rules inside comments
 		"nested-comment": {
-			{`\*/`, CommentMultiline, Pop(1)},
-			{`/\*`, CommentMultiline, Push()},
-			{`[^*/]+`, CommentMultiline, nil},
-			{`[*/]`, CommentMultiline, nil},
+			{`\*/`, CommentMultiline, Pop(1)}, // decrease comment level
+			{`/\*`, CommentMultiline, Push()}, // increase comment level
+			{`[^*/]+`, CommentMultiline, nil}, // comment content
+			{`[*/]`, CommentMultiline, nil},   // comment content
+		},
+
+		// rules inside any block
+		"block": {
+
+			/* nested block */
+			{`([\w\-\$\.]+)(\s*)(\{)`, ByGroups(NameFunction, Text, Punctuation), Push("block")},
+
+			/* brace escape */
+			{`\{`, Punctuation, Push("block")},
+
+			/* exit block OR brace escape */
+			{`\}`, Punctuation, Pop(1)},
 		},
 	},
 ))
